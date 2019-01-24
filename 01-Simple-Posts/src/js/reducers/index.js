d853ab56d2d4ddeeab8f6c7bcaaad78b03ab92f9
Boston Kenne <bostonkenne@localhost.localdomain>
2019-01-24 08:43:34 +0100

    20% up for 01

---
+++ 01-Simple-Posts/src/js/reducers/index.js
@@ -0,0 +1,11 @@
+import {ADD_ARTICLE}  from  '../constants/action-types'
+
+const initialState = {
+    articles: []
+}
+
+function  rootReducer(state=initialState, action){
+    return state;
+};
+
+export default rootReducer;
